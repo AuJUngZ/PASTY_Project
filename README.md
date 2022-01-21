e15c296d581f79d43ce84aa5e62da1d4130fe541
AuJung <nuttapong6280@gmail.com>
2022-01-21 17:24:13 +0700

    Prototype

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PASTY_Project
+
+1.เลือก role
+
+//role student
+1.ค้นหาหนังสือ ---> Booklist.txt(แยกหมวด)  --> BookName , Auther , Quatity , BookId
+2.ยืมหนังสือ ---> ListIssueBook.txt --> BookName , Std_Name, Std_Id , date (in-out)
+3.คืนหนังสือ ---> แก้ไข Booklist.txt , ListIssueBook.txt
+
+
+//role admin
+**Password (can be change)
+1.เพิ่ม & ลบ หนังสือ ---> Booklist.txt(แยกหมวด)  --> BookName , Auther , Quatity
+2.แก้ไขหนังสือ ---> Booklist.txt(แยกหมวด)
+3.ค้นหาหนังสือ ---> Booklist.txt(แยกหมวด)  --> BookName , Auther , Quatity
+
---
+++ library.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+class pasty{
+    public:
+        string BookName, Auther, Id;
+        int Quatity;
+
+    pasty()
+       {
+           BookName = "NO Book Name";
+           Auther = "No Author Name";
+           Id = "No Book ID";
+           Quatity = 0;
+       }
+          void Select_role();
+          void MainMenu_Student();
+          void MainMenu_Admin();
+          void SearchBook();
+          void IssueBook();
+          void ReturnBook();
+          void PassWord();
+          void AddBook();
+          void DeleteBook();
+
+};
+
+
+int main(){
+
+}
+
+    วิธีสร้าง Function
+//////////////////////////
+void pasty:: ชื่อFunctions(){
+
+}
+//////////////////////////
