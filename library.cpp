61b70e88d3a803285001fea4bc63f6e52d2eb940
AuJung <nuttapong6280@gmail.com>
2022-01-21 17:54:56 +0700

    Add Select_role Function

--- library.cpp
+++ library.cpp
@@ -34,9 +34,36 @@ int main(){
 
 }
 
-    วิธีสร้าง Function
+    /*วิธีสร้าง Function*/
 //////////////////////////
+/*
 void pasty:: ชื่อFunctions(){
 
 }
+*/
 //////////////////////////
+
+void pasty:: Select_role(){
+    int Choice;
+    cout<<"\n\t*********** PASTY MANAGEMENT SYSTEM ***********\n";
+    cout<<"\n\t\t>>Please Choose Any Option To login \n";
+    cout<<"\n\t\t1.Student\n\n\t\t2.Admin\n\n\t\t3.Close Application\n";
+    cout<<"\n\t\tEnter your choice : ";
+    cin >> Choice;
+    
+    if(Choice == 1){
+        system("cls");
+        MainMenu_Student()
+    }else if(Choice == 2){
+        system("cls");
+        MainMenu_Admin()
+    }else if(Choice == 3){
+        exit(0);
+    }else{
+        cout<<"\n\t\tPlease enter correct option :(";
+        getch();
+        system("CLS");
+        Select_role();
+    }
+        
+}
